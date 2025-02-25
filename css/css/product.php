e0c9401da12e341f69d81ed9604319d9d3e23a5d
Poovarasan2307 <poovarasank2307@gmail.com>
2025-02-25 18:53:46 +0530

    Add files via upload

---
+++ css/css/product.php
@@ -0,0 +1,47 @@
+<?php
+include('../config.php');
+$product_id = $_GET['id'];
+$query = "SELECT * FROM products WHERE id = $product_id";
+$result = $conn->query($query);
+$product = $result->fetch_assoc();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $product['name']; ?> - Organic Foods</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Organic Foods</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <h1><?php echo $product['name']; ?></h1>
+        <img src="../assets/images/<?php echo $product['image_path']; ?>" class="img-fluid" alt="<?php echo $product['name']; ?>">
+        <p><?php echo $product['description']; ?></p>
+        <p>$<?php echo $product['price']; ?></p>
+        <a href="#" class="btn btn-primary">Buy Now</a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
